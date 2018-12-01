a72338f1e71c8912b2980a6c2aaea63feb4e352b
Mac <zombietux@macui-macbook-pro-2.local>
2018-12-01 16:08:17 +0900

    Initial Commit

---
+++ LifeHacksApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  LifeHacksApp
+//
+//  Created by zombietux on 01/12/2018.
+//  Copyright © 2018 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
