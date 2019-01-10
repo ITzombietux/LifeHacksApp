167eab393ad3b389f3d0aaf893d1abba01ccf84e
zombietux <zombietux@Macui-MacBook-Pro-2.local>
2019-01-10 23:55:35 +0900

    Adding and Configuring Dynamic Table Views

---
+++ LifeHacksApp/TopQuestionsViewController.swift
@@ -0,0 +1,22 @@
+//
+//  TopQuestionsViewController.swift
+//  LifeHacksApp
+//
+//  Created by zombietux on 10/01/2019.
+//  Copyright © 2019 zombietux. All rights reserved.
+//
+
+import UIKit
+
+class TopQuestionsViewController: UIViewController, Stateful {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var stateController: StateController?
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+}
